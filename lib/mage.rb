444ed3ffa66cfe2b0714d5ad6eeda87df8b78c87
effect305 <effect305@mail.ru>
2016-04-18 12:43:14 +0300

    added model errors support

--- lib/mage.rb
+++ lib/mage.rb
@@ -49,11 +49,17 @@ class ActionController::Base
   rescue_from Mage::WizardRendered, with: :mage_return
 
   def render_mage(object, options = {})
+    puts "#{object}"
     unless object.mage_step == :done
       if request.method == 'GET'
         if options[:show_step] && params[:step].blank?
           mage_redirect(object, options[:show_step])
         else
+          if flash["#{object.model_name.name.downcase}_errors"]
+            flash["#{object.model_name.name.downcase}_errors"].each do |attribute, errors|
+              errors.each { |error| object.errors[attribute] << error }
+            end
+          end
           render "#{params[:controller]}/steps/#{object.mage_step}"
         end
       else
@@ -64,6 +70,7 @@ class ActionController::Base
   end
 
   def mage_redirect(object, show_step = false)
+    flash["#{object.model_name.name.downcase}_errors"] = object.errors.messages
     path = object.new_record? ? :new_polymorphic_path : :edit_polymorphic_path
     redirect_to send(path, object, step: (object.mage_step if show_step))
   end
--- lib/mage/version.rb
+++ lib/mage/version.rb
@@ -1,3 +1,3 @@
 module Mage
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
